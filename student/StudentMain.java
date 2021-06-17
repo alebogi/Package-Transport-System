4531aa8ee786d723445382cae692052801390924
alebogi <no.oneplayer@gmail.com>
2021-06-17 11:20:58 +0200

    initial commit

---
+++ student/StudentMain.java
@@ -0,0 +1,41 @@
+package student;
+
+import rs.etf.sab.*;
+import rs.etf.sab.operations.CityOperations;
+import rs.etf.sab.operations.CourierOperations;
+import rs.etf.sab.operations.CourierRequestOperation;
+import rs.etf.sab.operations.DistrictOperations;
+import rs.etf.sab.operations.GeneralOperations;
+import rs.etf.sab.operations.PackageOperations;
+import rs.etf.sab.operations.UserOperations;
+import rs.etf.sab.operations.VehicleOperations;
+import rs.etf.sab.tests.TestHandler;
+import rs.etf.sab.tests.TestRunner;
+
+
+
+public class StudentMain {
+
+    public static void main(String[] args) {
+        CityOperations cityOperations = new ba170390_CityOperations(); 
+        DistrictOperations districtOperations = new ba170390_DistrictOperations(); 
+        CourierOperations courierOperations = new ba170390_CourierOperations(); 
+        CourierRequestOperation courierRequestOperation = new ba170390_CourierRequestOperation();
+        GeneralOperations generalOperations = new ba170390_GeneralOperations();
+        UserOperations userOperations = new ba170390_UserOperations();
+        VehicleOperations vehicleOperations = new ba170390_VehicleOperations();
+        PackageOperations packageOperations = new ba170390_PackageOperations();
+
+        TestHandler.createInstance(
+                cityOperations,
+                courierOperations,
+                courierRequestOperation,
+                districtOperations,
+                generalOperations,
+                userOperations,
+                vehicleOperations,
+                packageOperations);
+
+        TestRunner.runTests();
+    }
+}
