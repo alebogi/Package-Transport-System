3146d58b7bf1823eb5bad1d73139e5104cdc7d89
alebogi <no.oneplayer@gmail.com>
2021-06-20 12:39:48 +0200

    districtOperations done

--- student/ba170390_DistrictOperations.java
+++ student/ba170390_DistrictOperations.java
@@ -5,6 +5,11 @@
  */
 package student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import rs.etf.sab.operations.DistrictOperations;
 
@@ -15,33 +20,124 @@ import rs.etf.sab.operations.DistrictOperations;
 public class ba170390_DistrictOperations implements DistrictOperations {
 
     @Override
-    public int insertDistrict(String string, int i, int i1, int i2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int insertDistrict(String name, int cityId, int xCord, int yCord) {
+        int id = -1;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="insert into District(Name, xCord, yCord, IdCity) values (?, ?, ?, ?)";
+        try (PreparedStatement stmt=conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);){
+            stmt.setString(1, name);
+            stmt.setInt(2, xCord);
+            stmt.setInt(3, yCord);
+            stmt.setInt(4, cityId);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            if(rs.next()){
+                id = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
     }
 
     @Override
-    public int deleteDistricts(String... strings) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int deleteDistricts(String... names) {
+        int numDeleted = 0;
+        if(names.length == 0)
+            return numDeleted;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="delete from District where Name=?";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){
+            for(int i = 0; i < names.length; i++){
+                String name = names[i];
+                stmt.setString(1, name);
+                numDeleted += stmt.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return numDeleted;    
     }
 
     @Override
-    public boolean deleteDistrict(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean deleteDistrict(int idDistrict) {
+        boolean res = false;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="delete from District where IdDistr=?";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){
+            stmt.setInt(1, idDistrict);
+            int tmp = stmt.executeUpdate();
+            if(tmp == 1)
+                res = true;
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
     }
 
     @Override
-    public int deleteAllDistrictsFromCity(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int deleteAllDistrictsFromCity(String nameOfTheCity) {
+        int numDeleted = 0;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="delete\n" +
+                    "from District\n" +
+                    "where IdCity=(\n" +
+                    "	select IdCity\n" +
+                    "	from City\n" +
+                    "	where Name=?\n" +
+                    ")";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){
+            stmt.setString(1, nameOfTheCity);
+            numDeleted += stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return numDeleted;
     }
 
     @Override
-    public List<Integer> getAllDistrictsFromCity(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Integer> getAllDistrictsFromCity(int idCity) {
+        List<Integer> list = new LinkedList<>();
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="select IdDistr from District where IdCity=?";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){  
+            stmt.setInt(1, idCity);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                list.add(rs.getInt(1));
+            }
+        } catch (SQLException ex) {
+           // Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return list;
     }
 
     @Override
     public List<Integer> getAllDistricts() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Integer> list = new LinkedList<>();
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="select IdDistr from District";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){            
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                list.add(rs.getInt(1));
+            }
+        } catch (SQLException ex) {
+           // Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return list;
     }
     
 }
--- student/ba170390_GeneralOperations.java
+++ student/ba170390_GeneralOperations.java
@@ -21,7 +21,7 @@ public class ba170390_GeneralOperations implements GeneralOperations {
     @Override
     public void eraseAll() {
         Connection conn=DB.getInstance().getConnection();
-        String [] names = {"Admin", "City", "Courier", "CourierRequest", "District", "Package", "User", "TransportOffer",  "Vehicle"}; 
+        String [] names = {"Admin", "Courier", "CourierRequest", "Package", "User", "TransportOffer",  "Vehicle", "District", "City"}; 
        
         try {
             
