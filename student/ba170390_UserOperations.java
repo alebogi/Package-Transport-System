0ac7c8dfb5f100fd2b28dc40ed20531824bfe67c
alebogi <no.oneplayer@gmail.com>
2021-06-20 14:46:55 +0200

    userOperations without declareAdmin

--- student/ba170390_UserOperations.java
+++ student/ba170390_UserOperations.java
@@ -5,7 +5,14 @@
  */
 package student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import rs.etf.sab.operations.UserOperations;
 
 /**
@@ -15,28 +22,119 @@ import rs.etf.sab.operations.UserOperations;
 public class ba170390_UserOperations implements UserOperations {
 
     @Override
-    public boolean insertUser(String string, String string1, String string2, String string3) {
+    public boolean insertUser(String userName, String firstName, String lastName, String password) {
+        Pattern patternFirstLetterUppercase = Pattern.compile("^([A-Z][a-zA-Z]*)");
+        Matcher matcher = patternFirstLetterUppercase.matcher(firstName);
+        boolean matchFound = matcher.find();
+        if(!matchFound) {        
+          System.out.println("Firstname of user must begin with uppercase letter.");
+          return false;
+        }
+        matcher = patternFirstLetterUppercase.matcher(lastName);
+        matchFound = matcher.find();
+        if(!matchFound) {        
+          System.out.println("Lastname of user must begin with uppercase letter.");
+          return false;
+        }
+        
+        Pattern patternPssword = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$");     
+        matcher = patternPssword.matcher(password);
+        matchFound = matcher.find();
+        if(!matchFound) {        
+          System.out.println("Password has to be longer than 8 characters. It should contain at least one number and one letter.");
+          return false;
+        }
+        
+        boolean res = false;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="insert into [User](Username, Firstname, Lastname, Password) values (?, ?, ?, ?)";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){
+            stmt.setString(1, userName);
+            stmt.setString(2, firstName);
+            stmt.setString(3, lastName);
+            stmt.setString(4, password);
+            int tmp = stmt.executeUpdate();
+            if(tmp == 1)
+                res = true;
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return res;
+    }
+
+    @Override
+    public int declareAdmin(String username) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public int declareAdmin(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Integer getSentPackages(String... userNames) {
+        int sum = 0;
+        
+        if(userNames.length == 0)
+            return null;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="select NumOfSentPckgs\n" +
+                    "from [User]\n" +
+                    "where Username=?";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){
+            for(int i = 0; i < userNames.length; i++){
+                String username = userNames[i];
+                stmt.setString(1, username);
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()){
+                    sum += rs.getInt(1);
+                }
+            }
+
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return sum;
     }
 
     @Override
-    public Integer getSentPackages(String... strings) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public int deleteUsers(String... userNames) {
+        int numDeleted = 0;
+        if(userNames.length == 0)
+            return 0;
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="delete\n" +
+                    "from [User]\n" +
+                    "where Username=?";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){
+            for(int i = 0; i < userNames.length; i++){
+                String username = userNames[i];
+                stmt.setString(1, username);
+                numDeleted += stmt.executeUpdate();
+            }
 
-    @Override
-    public int deleteUsers(String... strings) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        } catch (SQLException ex) {
+            //Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numDeleted;
     }
 
     @Override
     public List<String> getAllUsers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<String> list = new LinkedList<>();
+        
+        Connection conn=DB.getInstance().getConnection();
+        String query="select Username from [User]";
+        try (PreparedStatement stmt=conn.prepareStatement(query);){            
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                list.add(rs.getString(1));
+            }
+        } catch (SQLException ex) {
+           // Logger.getLogger(ba170390_CityOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return list;
     }
     
 }
